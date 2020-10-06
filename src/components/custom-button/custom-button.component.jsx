126b9a1ff3f7de23de14bb448571de74dd09add9
Juan Uribe <juanuribe80@gmail.com>
2020-10-06 17:26:06 -0400

    add redux, and implemented userReducer and userActions to header and app components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
-    return (
-        <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-            {children}
-        </button>
-    );
-}
+  return (
+    <button
+      type='button'
+      className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default CustomButton;
